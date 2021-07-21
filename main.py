4cf8f698cbc2f800e6ad2a36c7694c8aeb76c088
Mohammad Hadi Keshavarzi <mohake.keshavarzi@gmail.com>
2021-07-21 22:31:49 +0430

    first steps in Qt

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import sys
-from PyQt5.QtWidgets import QApplication , QWidget
+import os
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication , QMainWindow
+
+Form = uic.loadUiType(os.path.join(os.getcwd(),"mainForm.ui"))[0]
+
+class mainWindow(QMainWindow, Form):
+    def __init__(self):
+        super(mainWindow,self).__init__()
+        self.setupUi(self)
 
 app = QApplication(sys.argv)
-window= QWidget()
+#app.setStyle("Fusion")
 
+window= mainWindow()
 window.show()
+
 sys.exit(app.exec())
